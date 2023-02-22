30e0a38708a21b8abdeaa6a2c2437691cd8c0b9a
Prachi <prachikale136@gmail.com>
2023-02-22 23:41:58 +0530

    [Prachi] Add: calculate 2^ of the value

--- BasicProgram/BasicProgram/BasicCode.cs
+++ BasicProgram/BasicProgram/BasicCode.cs
@@ -10,29 +10,24 @@ namespace BasicProgram
     internal class BasicCode
     {
 
-        public int Year;
-        public void LeapYear()
+        public int N;
+        int i;
+        public void PowerOfValue()
         {
-            Console.WriteLine("Enter the Year");
-            Year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Value");
+            N = Convert.ToInt32(Console.ReadLine());
 
-            if ((Year % 400) == 0)
-            {
-                Console.WriteLine("Year is leap year");
-            }
-            else if ((Year % 100) == 0)
-            {
-                Console.WriteLine("Year is  Leap Year");
-            }
-            else if ((Year % 4) == 0)
-            {
-                Console.WriteLine("Year is leap year");
-            }
-            else 
-            {
-                Console.WriteLine("Year is not Leap year");
-            }
+            Console.WriteLine("Power of 2" +N+ "is: " +(Math.Pow(2, N)) );  //calculate the power of number
 
+            Console.WriteLine("Printing till Power VAlue");
+            for (i = 0; i <= N; i++)
+            {
+                Console.WriteLine("Power of 2" +i+ "is :" +(Math.Pow(2, i)));  //iterate the loop till the value of N and pribt the result
+            }
+            
+            //Console.WriteLine("Power of two:" +PowerOfTwo);
+
+            
         }
     }
 }
