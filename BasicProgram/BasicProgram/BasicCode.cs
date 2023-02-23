be06674a0ebd512324343b45d500fe8e9294264c
Prachi <prachikale136@gmail.com>
2023-02-23 15:04:42 +0530

    [Prachi] Add: Update code

--- BasicProgram/BasicProgram/BasicCode.cs
+++ BasicProgram/BasicProgram/BasicCode.cs
@@ -19,7 +19,7 @@ namespace BasicProgram
             number1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter the second number");
             number2= Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("/n/n");
+          
             Console.WriteLine("Before Swapping");
             Console.WriteLine("First Number" +number1);
             Console.WriteLine("second Number " + number2);
@@ -27,7 +27,7 @@ namespace BasicProgram
             temp = number1; //store number1 value into the temp variable
             number1= number2;  //store number 2 value into the number 1 variable
             number2= temp;  //and last store  temp value to the number 2 variable
-            Console.WriteLine("/n/n");
+            
             Console.WriteLine("After Swapping");
             Console.WriteLine("First number:" +number1);
             Console.WriteLine("Second Number:" + number2);
