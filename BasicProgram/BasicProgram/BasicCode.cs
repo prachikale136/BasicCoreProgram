11839a5d0c8560cd9d6ef544c9d6621b62e6f732
Prachi <prachikale136@gmail.com>
2023-02-23 15:15:19 +0530

    [Prachi] Add: Check whether number even or odd

--- BasicProgram/BasicProgram/BasicCode.cs
+++ BasicProgram/BasicProgram/BasicCode.cs
@@ -9,28 +9,21 @@ namespace BasicProgram
     
     internal class BasicCode
     {
-
-        public int number1;
-        public int number2;
-        int temp;
-        public void SwapTwoNumber()
+        public int input;
+        public void EvenOrOdd()
         {
-            Console.WriteLine("Enter the first number");
-            number1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the second number");
-            number2= Convert.ToInt32(Console.ReadLine());
-          
-            Console.WriteLine("Before Swapping");
-            Console.WriteLine("First Number" +number1);
-            Console.WriteLine("second Number " + number2);
+            Console.WriteLine("Enter number to check even or odd");
+            input = Convert.ToInt32(Console.ReadLine());
 
-            temp = number1; //store number1 value into the temp variable
-            number1= number2;  //store number 2 value into the number 1 variable
-            number2= temp;  //and last store  temp value to the number 2 variable
+            if ((input % 2) == 0)
+            {
+                Console.WriteLine("number is even Number");
+            }
+            else
+            {
+                Console.WriteLine("Number is Odd Number");
+            }
             
-            Console.WriteLine("After Swapping");
-            Console.WriteLine("First number:" +number1);
-            Console.WriteLine("Second Number:" + number2);
 
 
 
